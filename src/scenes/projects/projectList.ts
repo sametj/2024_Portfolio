568eac11fb9120fb06af97eb165427683d412b0b
Moses <mosesjames2000@gmail.com>
2024-10-15 23:48:13 -0400

    Progress

--- src/scenes/projects/projectList.ts
+++ src/scenes/projects/projectList.ts
@@ -4,15 +4,13 @@ export const PROJECTS = [
     Title: "SipNPlay Codedex 24hr Hackathon Project",
     Image: "/Sip&Play.png",
     Stack: "React | TypeScript | Vite | Three.js",
-    Description: "klajsffffhllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll",
     GitHub: "https://github.com/Prometheus-Spark/hackathon-2024",
     Link: "https://sip-n-play-nyc.vercel.app/"
   },
   {
     Title: "Trinity Competitive Racing",
-    Image: "/TCR.png",
+    Image: "/tcr.jpeg",
     Stack: "React | TypeScript | Vite | SCSS ",
-    Description: "lorem",
     GitHub: "https://github.com/sametj/trinity-competitive-racing",
     Link: "https://trinitycompetitiveracing.netlify.app/"
   },
@@ -20,7 +18,6 @@ export const PROJECTS = [
     Title: "3D Portfolio",
     Image: "/Portfolio.png",
     Stack: "React | JavaScript | Vite | Three.js",
-    Description: "lorem",
     GitHub: "https://github.com/sametj/New3dPortfolio",
     Link: "https://www.same-teejay.tech/"
   },
@@ -28,7 +25,6 @@ export const PROJECTS = [
     Title: "Astral Explorer",
     Image: "/javaproject.png",
     Stack: "Java | JSP | MySQL | Tomcat | HTML | CSS",
-    Description: "lorem",
     GitHub: "https://github.com/sametj/Dynamic-Astral-Explorer",
   }
 ]
