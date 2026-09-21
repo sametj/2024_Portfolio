3bcb32dba03000ef278f94b70bcdd2056271a9df
TJ <you@example.com>
2026-09-21 06:29:05 -0400

    Updated project section

--- src/scenes/projects/projectList.ts
+++ src/scenes/projects/projectList.ts
@@ -1,30 +1,24 @@
 export const PROJECTS = [
-
   {
     Title: "SipNPlay Codedex 24hr Hackathon Project",
     Image: "/Sip&Play.png",
     Stack: "React | TypeScript | Vite | Three.js",
     GitHub: "https://github.com/Prometheus-Spark/hackathon-2024",
-    Link: "https://sip-n-play-nyc.vercel.app/"
+    Link: "https://sip-n-play-nyc.vercel.app/",
   },
   {
     Title: "Trinity Competitive Racing",
     Image: "/tcr.jpeg",
     Stack: "React | TypeScript | Vite | SCSS ",
     GitHub: "https://github.com/sametj/trinity-competitive-racing",
-    Link: "https://trinitycompetitiveracing.netlify.app/"
+    Link: "https://trinitycompetitiveracing.netlify.app/",
   },
   {
     Title: "3D Portfolio",
     Image: "/Portfolio.png",
     Stack: "React | JavaScript | Vite | Three.js",
     GitHub: "https://github.com/sametj/New3dPortfolio",
-    Link: "https://www.same-teejay.tech/"
+    Link: "https://teejay-portfolio-old.vercel.app/",
   },
-  {
-    Title: "Astral Explorer",
-    Image: "/javaproject.png",
-    Stack: "Java | JSP | MySQL | Tomcat | HTML | CSS",
-    GitHub: "https://github.com/sametj/Dynamic-Astral-Explorer",
-  }
-]
+];
+
